ce31c6879c577014199a3f5714931d07855ec33d
MichalReclaw <michal_reclaw@o2.pl>
2018-03-19 19:55:48 +0100

    adding Rate class

---
+++ src/main/java/api/Rate.java
@@ -0,0 +1,40 @@
+package api;
+
+public class Rate {
+	private String no;
+	private String effectiveDate;
+	private Double bid;
+	private Double ask;
+
+	public String getNo() {
+		return no;
+	}
+
+	public void setNo(String no) {
+		this.no = no;
+	}
+
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(String effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public Double getBid() {
+		return bid;
+	}
+
+	public void setBid(Double bid) {
+		this.bid = bid;
+	}
+
+	public Double getAsk() {
+		return ask;
+	}
+
+	public void setAsk(Double ask) {
+		this.ask = ask;
+	}
+}
